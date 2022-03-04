efcae4d9760ca35fc9492445705e4afd378a0b0f
Shan Yang 楊佩姗 <ShanYang@corpnet.auo.com>
2022-03-04 16:31:06 +0800

    get-experiments done

--- main/Main.py
+++ main/Main.py
@@ -1,42 +1,67 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from utils.CryptoUtil import decode_key, make_project_folder, save_config_as_json
+import utils.CryptoUtil
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/creat-project-by-key', methods=['POST'])
 def create_project_by_key():
+    '''
+    decode key to get config
+    create a new project folder/ or already existed
+    save config as json
+    '''
     data = request.get_json()
     if not data:
         return "There is no data."
 
     projectName = data['name']
-    config = decode_key(data['key'])
+    config = utils.CryptoUtil.decode_key(data['key'])
     if not config:
         return "Wrong key format."
 
-    projectsList = make_project_folder(projectName)
+    projectsList = utils.CryptoUtil.create_project(projectName)
     if not projectsList:
         return "Invalid project name."
     
-    configPath = save_config_as_json(projectName, config)
+    configPath = utils.CryptoUtil.save_config_as_json(projectName, config)
     if not configPath:
         return "Config save error."
-
     
-
-    # create project folder projects/{{projectName}}/experiments/202203021355.json
-    # decode solution key and save experiment config
-    # return jsonify(config)
-
-    
-    return jsonify(projectsList)
+    return {"projects": projectsList}
 
 @app.route('/get-experiments', methods=['POST'])
 def get_experiments():
-    # return experiments config
-    return ''
+    '''
+    return experiment config
+    '''
+    data = request.get_json()
+    if not data:
+        return "There is no data."
+    
+    projectName = data['name']
+    projectFolderPath = utils.CryptoUtil.find_project(projectName)
+    if not projectFolderPath:
+        return "Project does not exist."
+    elif projectFolderPath == False:
+        return "Project does not exist."
+
+    config = utils.CryptoUtil.get_config(projectFolderPath)
+
+    return config
+
+
+@app.route('/check-dataset', methods=['POST'])
+def check_dataset():
+    '''
+    check dataset status: {uploaded: bool, labeled: bool, split: bool}
+    '''
+    data = request.get_json()
+    if not data:
+        return "There is no data."
+
+    return data
 
 def main():
     app.run(host='0.0.0.0', port=5000)
--- utils/CryptoUtil/__init__.py
+++ utils/CryptoUtil/__init__.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on FRI MAR 4 17:00:00 2021
+@author: ShanYang
+"""
+
 import json, jwt, os
 from datetime import datetime
 
 key = 'auo'
 algorithm = 'HS256'
+projectsPath = f"./projects"
 
 def decode_key(token):
     try:
@@ -11,22 +19,50 @@ def decode_key(token):
     except:
         return None
 
-def make_project_folder(projectName):
+def create_project(projectName):
     try:
-        if not os.path.exists(f"./projects/{projectName}"):
-            os.makedirs(f"./projects/{projectName}/experiments")
-            os.makedirs(f"./projects/{projectName}/runs")
-        projectsList = os.listdir(f"./projects/")
+        projectPath = f"{projectsPath}/{projectName}"
+        if not os.path.exists(projectPath):
+            os.makedirs(f"{projectPath}/experiments")
+            os.makedirs(f"{projectPath}/runs")
+        projectsList = os.listdir(projectsPath)
         return projectsList
     except:
         return None
+
+def find_project(projectName):
+    try:
+        projectPath = f"{projectsPath}/{projectName}"
+        if os.path.exists(projectPath):
+            return projectPath
+        else:
+            return False
+    except:
+        return none
 
 def save_config_as_json(projectName, config):
     try:
         jsonName = datetime.now().strftime('%Y%m%d%H%M%S')
         with open(f"./projects/{projectName}/experiments/{jsonName}.json", 'w') as jsonFile:
-            json.dump(config, jsonFile)
+            json.dump(config, jsonFile, indent=4)
         return f"./projects/{projectName}/{jsonName}.json"
     except:
         return None
+
+def get_config(projectFolderPath):
+    try:
+        configsList = os.listdir(f"{projectFolderPath}/experiments")
+        configDict = {}
+        for configFile in configsList:
+            with open(f"{projectFolderPath}/experiments/{configFile}") as jsonFile:
+                config = json.load(jsonFile)
+                configDict[str(configFile.split('.')[0])] = config
+        return configDict
+    except:
+        return None
+
+    
+    
+
+
 
