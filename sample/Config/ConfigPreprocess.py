052ca53a4f79c17ea5927bf52d9c994d4673de04
Shan Yang 楊佩姗 <ShanYang@corpnet.auo.com>
2022-03-14 13:41:50 +0800

    update jwt

--- sample/Config/ConfigPreprocess.py
+++ sample/Config/ConfigPreprocess.py
@@ -1,6 +1,3 @@
 class PreprocessPara:
-    normalize = {'switch': False, 
-                'mode': 0,
-                'mean': [],
-                'std': []}
+    normalize = {'switch': False, 'mode': 0, 'mean': [], 'std': []}
     imageSize = [224, 224]
