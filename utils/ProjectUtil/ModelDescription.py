469f057feb5e39b7d5f32adb6d189cf1fe6ee5c5
JoeJHHuang <joe.jh.huang@auo.com>
2022-07-04 02:13:50 +0000

    modelDescription description -> introduction

--- utils/ProjectUtil/ModelDescription.py
+++ utils/ProjectUtil/ModelDescription.py
@@ -3,84 +3,84 @@ modelDescription = {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★☆☆☆",
         "trainSpeed:": "★★☆☆☆",
-        "description": "小型網路，靈活卷積尺寸及層數，可適用多樣特徵。"
+        "introduction": "小型網路，靈活卷積尺寸及層數，可適用多樣特徵。"
     },
     "auo_classic_deep_model": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★★☆",
         "trainSpeed:": "★★★★☆",
-        "description": "小型網路，固定卷積數量與架構，增加網路深度，取得更深層特徵。"
+        "introduction": "小型網路，固定卷積數量與架構，增加網路深度，取得更深層特徵。"
     },
     "auo_anti_degeneration_model": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★★★",
         "trainSpeed:": "★★★☆☆",
-        "description": "深度網路，加強卷積層連結，保留底層特徵資訊，並大幅提升可訓練程度。"
+        "introduction": "深度網路，加強卷積層連結，保留底層特徵資訊，並大幅提升可訓練程度。"
     },
     "auo_transform_aggregate_model": {
         "dataNeeded:": "★★★★☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★☆☆☆",
-        "description": "深度網路，擴寬模型寬度，取得不同尺寸特徵。"
+        "introduction": "深度網路，擴寬模型寬度，取得不同尺寸特徵。"
     },
     "auo_narrow_intensive_model": {
         "dataNeeded:": "★★★★☆",
         "easeOfUsage:": "★★☆☆☆",
         "trainSpeed:": "★☆☆☆☆",
-        "description": "深度網路，多重反覆利用特徵，模型收斂速度快。"
+        "introduction": "深度網路，多重反覆利用特徵，模型收斂速度快。"
     },
     "auo_feature_enhance_model_a": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★☆☆☆",
-        "description": "深度網路，加強特定維度權重，增進有效樣本學習。"
+        "introduction": "深度網路，加強特定維度權重，增進有效樣本學習。"
     },
     "auo_feature_enhance_model_b": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★☆☆☆",
-        "description": "深度網路，加強多重維度權重，增進有效樣本學習。"
+        "introduction": "深度網路，加強多重維度權重，增進有效樣本學習。"
     },
     "auo_feature_enhance_model_c": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★☆☆☆",
-        "description": "深度網路，結合友達特徵增進模型A、效果B，增進有效樣本學習。"
+        "introduction": "深度網路，結合友達特徵增進模型A、效果B，增進有效樣本學習。"
     },
     "auo_faster_convergency_model": {
         "dataNeeded:": "★★★★☆",
         "easeOfUsage:": "★★☆☆☆",
         "trainSpeed:": "★★★☆☆",
-        "description": "深度網路，平衡模型深度、寬度、解析度，適合訓練多樣性高的資料。"
+        "introduction": "深度網路，平衡模型深度、寬度、解析度，適合訓練多樣性高的資料。"
     },
     "auo_unrestricted_powerful_model": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★★☆",
         "trainSpeed:": "★★★★☆",
-        "description": "深度網路，彈性卷積與層數設計，在各種資料中有良好的適應性。"
+        "introduction": "深度網路，彈性卷積與層數設計，在各種資料中有良好的適應性。"
     },
     "auo_cross_connection_model": {
         "dataNeeded:": "★★★☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★★☆☆",
-        "description": "深度網路，增加跨卷積層連結，減少訓練時長、參數及保留底層特徵。"
+        "introduction": "深度網路，增加跨卷積層連結，減少訓練時長、參數及保留底層特徵。"
     },
     "auo_feature_reuse_model": {
         "dataNeeded:": "★★☆☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★★★☆",
-        "description": "輕量化網路，重用底層特徵，可用於規格較小的硬體。"
+        "introduction": "輕量化網路，重用底層特徵，可用於規格較小的硬體。"
     },
     "auo_lighten_efficient_model": {
         "dataNeeded:": "★★☆☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★★★☆",
-        "description": "輕量化網路，加速推理階段速度。"
+        "introduction": "輕量化網路，加速推理階段速度。"
     },
     "auo_horizontal_sample_model": {
         "dataNeeded:": "★★☆☆☆",
         "easeOfUsage:": "★★★☆☆",
         "trainSpeed:": "★★★★☆",
-        "description": "輕量化網路，壓縮計算量及參數，擁有多樣資料適應性。"
+        "introduction": "輕量化網路，壓縮計算量及參數，擁有多樣資料適應性。"
     }
 }
